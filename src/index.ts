30840f1e8c7d12667c8c5d836d8e0f66557f97dd
SYNAPSYS\LahiruR <lahiru.rathnasheela@synapsys.lk>
2024-06-08 00:55:33 +0530

    restaurant management finished

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,19 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import { v2 as cloudinary } from "cloudinary";
 
 import myUserRouter from "./routes/MyUserRoutes";
+import myrestaurantRouter from "./routes/MyRestaurantRoutes";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Connected to DB");
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
@@ -18,6 +26,7 @@ app.use("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRouter);
+app.use("/api/my/restaurant", myrestaurantRouter);
 
 app.listen(8000, () => {
   console.log("Server started on localhost: 8000");
